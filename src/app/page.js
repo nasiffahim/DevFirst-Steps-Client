103740ec1f576f550fc3f7b16af175a168209b41
Abdul Aziz <abdulaziz758015@gmail.com>
2025-09-16 12:55:50 +0600

    create hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
+import HeroSection from "./HeroSection";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center text-center p-24">
-      <h1 className="text-4xl">Welcome to DevFirst Steps</h1>
-      <p>One place to find beginner-friendly open-source projects</p>
-      <p>Aziz is connected</p>
+    <div className="min-h-screen">
+     <HeroSection></HeroSection>
     </div>
   );
 }

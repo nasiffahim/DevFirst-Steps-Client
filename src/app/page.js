8eae892f05d301d88003871d3bb0f8de56717e58
Abdul Aziz <abdulaziz758015@gmail.com>
2025-09-18 18:51:20 +0600

    added projects section in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "../Components/home/HeroSection";
+import ProjectsPage from "./projects/[id]/page";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
      <HeroSection></HeroSection>
+     <ProjectsPage></ProjectsPage>
     </div>
   );
 }

6aafdc69aae970f2e4d133358697ec36fb919abb
Nasif Wasek <nasif.wasek@gmail.com>
2025-09-19 10:16:14 +0600

    issue fix for deployment

---
+++ src/app/dashboard/my-projects/[id]/edit/page.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Edit Details Page (Coming Soon)</h1>
+    </div>
+  );
+}
---
+++ src/app/projects/categories/[id]/page.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Category Page (Coming Soon)</h1>
+    </div>
+  );
+}

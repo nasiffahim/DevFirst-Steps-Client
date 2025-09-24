e042494ec867e43e998a96b73c5a3642c4b12ef0
Shohel-Raj <shohel87.dev@gmail.com>
2025-09-25 03:23:28 +0600

    Profile page error resolved

--- src/app/dashboard/profile/page.jsx
+++ src/app/dashboard/profile/page.jsx
@@ -1,10 +1,12 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { use, useState } from "react";
+import useAuth from "../../hooks/useAuth";
 
 export default function ProfilePage() {
   const [tab, setTab] = useState("blogs"); // default tab
+  const { user, loading } = useAuth();
 
   const projects = [
     { title: "Python Explorer", link: "#" },
@@ -33,6 +35,21 @@ export default function ProfilePage() {
       info: "Let’s talk about new projects in the community!",
     },
   ];
+   if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>Loading profile...</p>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>No user logged in.</p>
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen bg-gray-50">
@@ -40,11 +57,13 @@ export default function ProfilePage() {
       <header className="bg-gray-800 text-white py-6 px-4">
         <div className="max-w-6xl mx-auto flex items-center justify-between">
           <div className="flex items-center space-x-4">
-            <div className="w-16 h-16 rounded-full bg-gray-600"></div>
+            <div className="w-16 h-16 rounded-full bg-gray-600">
+              <img src={user.photoURL} alt="Profile Picture"  className="rounded-full w-full h-full" />
+            </div>
             <div>
-              <h1 className="text-xl font-semibold">JohnDoe</h1>
+              <h1 className="text-xl font-semibold">{user.displayName}</h1>
               <p className="text-sm text-gray-300">
-                Open source enthusiast and software engineer
+              {user.email}
               </p>
             </div>
           </div>
